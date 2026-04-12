885a40f2b8bad0c864103c600af8cb70ad6676ee
kevaltrivedi_8899 <keval@itoneclick.com>
2026-04-12 18:58:58 +0530

    feat: mongoose and global config setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
+const mongoUri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/rectify-api';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(mongoUri),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  // app.enableCors({
+  //   origin: corsOrigins,
+  //   credentials: true,
+  // });
+
+  // Set global prefix
+  app.setGlobalPrefix('/api');
+
+  // Global pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  // Start server
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
